b22a8507528eb67d17f22b45f58bcb0a063e22ac
Apple <apple@Apples-MacBook-Pro.local>
2026-07-17 10:54:14 +0530

    placeholders task

--- scripts/placeholders.js
+++ scripts/placeholders.js
@@ -2,7 +2,7 @@ let placeholders = null;
 
 export async function getPlaceholder(key) {
   if (!placeholders) {
-    const response = await fetch("/docs/placeholders.json");
+    const response = await fetch("/docs/placeholders/placeholders.json");
 
     if (!response.ok) {
       throw new Error("Could not load placeholders");
